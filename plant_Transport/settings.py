e18cd31b5f68269bf393d9ff03ebf2f47000bb92
Siva <105603060+Sambasivaraopopuri@users.noreply.github.com>
2024-01-09 15:06:42 +0530

    Update settings.py

--- plant_Transport/settings.py
+++ plant_Transport/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'plant_Transport.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql', 
@@ -90,16 +90,16 @@ WSGI_APPLICATION = 'plant_Transport.wsgi.application'
 #         'PORT': '3306',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'PlantTransport',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'PlantTransport',
 #       'USER': 'Siva',
 #       'PASSWORD': 'Siva01@',
 #       'HOST': '34.16.195.130',
-        'PORT': '5432'
-    }
-}
+#         'PORT': '5432'
+#     }
+# }
 .
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
